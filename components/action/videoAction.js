15530f73f168837bd12bcf9840add12ba67bcbbe
Milan Kumar <66168933+Quasar0007@users.noreply.github.com>
2021-06-14 20:05:30 +0530

    Delete

--- components/action/videoAction.js
+++
@@ -1,7 +0,0 @@
-import IO from 'socket.io-client';
-import Peer from 'react-native-peerjs';
-import {ADD_STREAM, MY_STREAM, ADD_REMOTE_STREAM} from './type';
-
-export const API_URI = 'http://192.168.29.166:5000';
-
-export const JOIN_ROOM = () => async(dispatch) => {};
